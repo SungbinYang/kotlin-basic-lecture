392efca55f3fa7976b0815356debde8e6d12c480
SungbinYang <ysb05222@naver.com>
2026-03-04 18:39:10 +0900

    feat: 코틀린에서 변수를 다루는 방법 자바 샘플 코드 작성

---
+++ src/main/java/me/sungbin/lec01/Lec01Main.java
@@ -0,0 +1,17 @@
+package me.sungbin.lec01;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Lec01Main {
+    static void main() {
+        long number1 = 10L;
+        final long number2 = 10L;
+
+        Long number3 = 1_000L;
+        Person person = new Person("양성빈");
+
+        final List<Integer> numbers = Arrays.asList(1, 2, 3);
+//        numbers.add(4);
+    }
+}
